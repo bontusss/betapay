8cbfa20f1b0fe78270c70900f41873af21897223
bontusss <ikwecheghu@gmail.com>
2022-03-27 11:12:12 +0100

    refactoring navbar

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { dom } from "@fortawesome/fontawesome-svg-core";
+import "vue-float-menu/dist/vue-float-menu.css";
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,11 @@
 import IntroVue from '../components/Intro.vue';
 import LearnVue from '../components/Learn.vue';
 import ActionBannerVue from '../components/ActionBanner.vue';
+import FloatNavbarVue from '../components/FloatNavbar.vue';
 </script>
 
 <template>
+    <FloatNavbarVue />
     <IntroVue class="margin" />
     <LearnVue class="margin" />
     <ActionBannerVue class="margin" />
